51e9f0a2ac51f96b746e324424d4afc1f674d7fe
Siddharth Diwan <sidwan02@gmail.com>
2021-01-15 20:29:57 -0500

    added task scheduler capability

---
+++ scheduled_scrape_merge.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Python37\python.exe" "C:\Users\sidwa\OneDrive\OneDriveNew\Personal\Sid\Brown University\Internships\ISB\DIRI DS\privacy_data_sentiment_analysis\scheduled_scrape_merge.py"
---
+++ scheduled_scrape_merge.py
@@ -0,0 +1,6 @@
+# https://towardsdatascience.com/automate-your-python-scripts-with-task-scheduler-661d0a40b279
+from privacy_history_multithreading import privacy_analysis_multithreading
+from merge_csv import merge_all_csv
+
+privacy_analysis_multithreading()
+merge_all_csv()
